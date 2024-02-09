c2d41aaa3de77a1ab6f04ed2efbcabc7b1eb6782
minseok-oh <minseok.dev@gmail.com>
2024-02-09 13:05:45 +0900

    feat: cors 해결 및 end point 설정

--- src/main/java/com/maps/coin/config/WebSocketConfig.java
+++ src/main/java/com/maps/coin/config/WebSocketConfig.java
@@ -17,13 +17,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/room");
+        config.setApplicationDestinationPrefixes("/room");
     }
 
     @Override
