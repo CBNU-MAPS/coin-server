006a1a2907257d1eb36c00020964d825d03a1157
minseok-oh <minseok.dev@gmail.com>
2024-02-14 21:40:31 +0900

    fix: message convert를 위한 getter 추가

--- src/main/java/com/maps/coin/dto/GamerInfoResponse.java
+++ src/main/java/com/maps/coin/dto/GamerInfoResponse.java
@@ -3,7 +3,9 @@ package com.maps.coin.dto;
 import com.maps.coin.domain.user.Gamer;
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class GamerInfoResponse {
     private List<GamerResponse> users;
 
