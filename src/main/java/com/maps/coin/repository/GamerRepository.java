5c709cecd4801a72627d8aa072521a25bce68f4d
minseok-oh <minseok.dev@gmail.com>
2024-02-15 21:54:18 +0900

    chore: setter 및 불필요한 부분 삭제

--- src/main/java/com/maps/coin/repository/GamerRepository.java
+++ src/main/java/com/maps/coin/repository/GamerRepository.java
@@ -1,10 +1,7 @@
 package com.maps.coin.repository;
 
 import com.maps.coin.domain.user.Gamer;
-import java.util.List;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GamerRepository extends JpaRepository<Gamer, UUID> {
-    List<Gamer> findByRoom_RoomId(UUID id);
+public interface GamerRepository extends JpaRepository<Gamer, String> {
 }
