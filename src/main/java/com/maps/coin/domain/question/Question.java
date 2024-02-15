5828239283b81c98df9f29e535d3dda6b3fd796b
minseok-oh <minseok.dev@gmail.com>
2024-02-15 21:51:29 +0900

    feat: erd를 통한 table 연결

--- src/main/java/com/maps/coin/domain/question/Question.java
+++ src/main/java/com/maps/coin/domain/question/Question.java
@@ -1,12 +1,18 @@
 package com.maps.coin.domain.question;
 
 import com.maps.coin.domain.BaseEntity;
+import com.maps.coin.domain.room.Problem;
+import com.maps.coin.domain.user.Answer;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -28,6 +34,12 @@ public class Question extends BaseEntity {
 
     @Column(name = "option")
     private String[] option;
+
+    @OneToMany(mappedBy="question", cascade = CascadeType.ALL)
+    private List<Problem> problems = new ArrayList<>();
+
+    @OneToMany(mappedBy="question", cascade = CascadeType.ALL)
+    private List<Answer> answers = new ArrayList<>();
 
     @Builder
     public Question(String type, String question, String[] option) {
--- src/main/java/com/maps/coin/domain/user/Gamer.java
+++ src/main/java/com/maps/coin/domain/user/Gamer.java
@@ -2,15 +2,16 @@ package com.maps.coin.domain.user;
 
 import com.maps.coin.domain.BaseEntity;
 import com.maps.coin.domain.room.Room;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +22,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "gamer")
 public class Gamer extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private String id;
 
     @Column(name = "name", nullable = false)
     private String name;
@@ -34,9 +34,14 @@ public class Gamer extends BaseEntity {
     @JoinColumn(name = "room_id", referencedColumnName = "id")
     private Room room;
 
+    @OneToMany(mappedBy="gamer", cascade = CascadeType.ALL)
+    private List<Answer> answers = new ArrayList<>();
+
     @Builder
-    public Gamer(String name, Integer avatar) {
+    public Gamer(String id, String name, Integer avatar, Room room) {
+        this.id = id;
         this.name = name;
         this.avatar = avatar;
+        this.room = room;
     }
 }
