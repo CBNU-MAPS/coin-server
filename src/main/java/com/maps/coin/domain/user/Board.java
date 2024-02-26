3a628f86dcb8c8cabc2bbd4075af1090ab9ea772
minseok-oh <minseok.dev@gmail.com>
2024-02-26 22:43:31 +0900

    feat: 유저 board 관리 및 같은 선택한 유저 찾기

---
+++ src/main/java/com/maps/coin/domain/user/Board.java
@@ -0,0 +1,31 @@
+package com.maps.coin.domain.user;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.util.Pair;
+
+@Getter
+@Setter
+public class Board {
+    Map<Long, Pair<Integer, Integer>> position;
+    List<List<Answer>> board;
+
+    public Board(List<Answer> answers, Integer size) {
+        this.position = new HashMap<>();
+        this.board = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            List<Answer> row = answers.subList(i * size, (i + 1) * size);
+
+            for (int j = 0; j < size; j++) {
+                Answer answer = row.get(j);
+                this.position.put(answer.getId(), Pair.of(i, j));
+            }
+            this.board.add(new ArrayList<>(row));
+        }
+    }
+}
