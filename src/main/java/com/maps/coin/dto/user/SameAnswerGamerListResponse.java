bfb9abec84a07a35e6a5ce593d314d1cd0e78d41
minseok-oh <minseok.dev@gmail.com>
2024-02-27 15:17:17 +0900

    fix: gamer list에서 gamer response list로 변동

--- src/main/java/com/maps/coin/dto/user/SameAnswerGamerListResponse.java
+++ src/main/java/com/maps/coin/dto/user/SameAnswerGamerListResponse.java
@@ -4,13 +4,15 @@ import com.maps.coin.domain.user.Gamer;
 import com.maps.coin.dto.answer.AnswerResponse;
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class SameAnswerGamerListResponse {
     AnswerResponse answer;
-    List<Gamer> selectedUsers;
+    List<GamerResponse> selectedUsers;
 
     @Builder
-    public SameAnswerGamerListResponse(AnswerResponse answer, List<Gamer> users) {
+    public SameAnswerGamerListResponse(AnswerResponse answer, List<GamerResponse> users) {
         this.answer = answer;
         this.selectedUsers = users;
     }
