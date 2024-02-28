d65a0d844a79d9eed0c6bb91545faa4a1806e4ce
minseok-oh <minseok.dev@gmail.com>
2024-02-29 00:10:53 +0900

    feat: status board 요청에 대한 코드 작성

--- src/main/java/com/maps/coin/service/GameService.java
+++ src/main/java/com/maps/coin/service/GameService.java
@@ -1,10 +1,14 @@
 package com.maps.coin.service;
 
+import com.maps.coin.domain.question.Question;
 import com.maps.coin.domain.room.Room;
 import com.maps.coin.domain.user.Answer;
 import com.maps.coin.domain.user.Board;
 import com.maps.coin.domain.user.Gamer;
+import com.maps.coin.dto.board.StatusBoardListResponse;
+import com.maps.coin.dto.board.StatusBoardResponse;
 import com.maps.coin.dto.user.GamerResponse;
+import com.maps.coin.repository.QuestionRepository;
 import com.maps.coin.repository.RoomRepository;
 import jakarta.transaction.Transactional;
 import java.util.ArrayList;
@@ -21,8 +25,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GameService {
     private final RoomRepository roomRepository;
+    private final QuestionRepository questionRepository;
 
     private Map<String, Board> boards = new HashMap<>();
+    private Map<UUID, List<StatusBoardResponse>> statusBoards = new HashMap<>();
 
     private void createGamerBoard(Gamer gamer, Integer size) {
         List<Answer> answers = gamer.getAnswers();
@@ -58,6 +64,21 @@ public class GameService {
                 gamerResponseList.add(GamerResponse.builder().name(g.getName()).avatar(g.getAvatar()).build());
             }
         });
+
+        List<StatusBoardResponse> statusBoard = statusBoards.get(roomId);
+        Question question = questionRepository.findById(questionId).orElse(null);
+        if (question != null) {
+            statusBoard.add(StatusBoardResponse.builder().answer(answer).question(question.getQuestion())
+                    .selectedUsers(gamerResponseList).build());
+            statusBoards.put(roomId, statusBoard);
+        }
         return gamerResponseList;
+    }
+
+    public List<StatusBoardResponse> findStatusBoardList(UUID roomId) {
+        if (!statusBoards.containsKey(roomId)) statusBoards.put(roomId, new ArrayList<>());
+
+        List<StatusBoardResponse> statusBoard = statusBoards.get(roomId);
+        return statusBoard;
     }
 }
