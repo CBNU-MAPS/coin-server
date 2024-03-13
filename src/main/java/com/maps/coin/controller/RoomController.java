5886795e5bdd048bf8f3ca4041a90781a2008974
minseok-oh <minseok.dev@gmail.com>
2024-03-13 21:50:50 +0900

    feat: room 정보 및 user 정보 api 개발

--- src/main/java/com/maps/coin/controller/RoomController.java
+++ src/main/java/com/maps/coin/controller/RoomController.java
@@ -3,6 +3,10 @@ package com.maps.coin.controller;
 import com.maps.coin.domain.room.Room;
 import com.maps.coin.dto.room.CreateRoomRequest;
 import com.maps.coin.dto.room.CreateRoomResponse;
+import com.maps.coin.dto.room.RoomInfoResponse;
+import com.maps.coin.dto.user.GamerInfoResponse;
+import com.maps.coin.dto.user.GamerResponse;
+import com.maps.coin.service.GamerService;
 import com.maps.coin.service.RoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,11 +14,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -22,9 +30,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Room", description = "Room API")
 public class RoomController {
     private final RoomService roomService;
+    private final GamerService gamerService;
 
     @PostMapping("/api/room")
-    @Operation(summary = "Post Room Information", description = "방을 생성하고 정보를 전달한다.")
+    @Operation(summary = "Post Room Information", description = "방을 생성한다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "방 생성에 성공했습니다.",
                     content = {@Content(schema = @Schema(implementation = Room.class))}),
@@ -34,5 +43,31 @@ public class RoomController {
         Room savedRoom = roomService.save(request);
         CreateRoomResponse createdRoom = roomService.createRoom(savedRoom);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdRoom);
+    }
+
+    @GetMapping("/api/room")
+    @Operation(summary = "Get Room Information", description = "방 정보를 보내준다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "방 정보 검색에 성공했습니다.",
+                    content = {@Content(schema = @Schema(implementation = Room.class))}),
+    })
+    public ResponseEntity<RoomInfoResponse> findRoom(@RequestParam String roomCode) {
+        if (!roomService.readRoomAccessPermission(UUID.fromString(roomCode))) {
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } else {
+            RoomInfoResponse roomInfo = roomService.readRoom(UUID.fromString(roomCode));
+            return ResponseEntity.status(HttpStatus.OK).body(roomInfo);
+        }
+    }
+
+    @GetMapping("/api/users")
+    @Operation(summary = "Get Users Information", description = "방에 존재하는 유저 정보를 전달한다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저 정보 검색에 성공했습니다.",
+                    content = {@Content(schema = @Schema(implementation = Room.class))}),
+    })
+    public ResponseEntity<GamerInfoResponse> findUsers(@RequestParam String roomCode) {
+        List<GamerResponse> gamers = gamerService.read(UUID.fromString(roomCode));
+        return ResponseEntity.status(HttpStatus.OK).body(GamerInfoResponse.builder().users(gamers).build());
     }
 }
